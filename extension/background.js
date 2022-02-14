792ce09548fbeef4771fbb5f269446a1e888164c
apollorion <joey@apollorion.com>
2022-02-14 17:36:20 -0500

    better service, better pizza, papa johns

--- extension/background.js
+++ extension/background.js
@@ -1,31 +1,43 @@
-chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-    chrome.bookmarks.search(text, (bookmarks) => {
-        const suggestions = bookmarks.map((bookmark) => {
-            if (
-                bookmark.url !== undefined &&
-                bookmark.title !== undefined &&
-                bookmark.url !== "" &&
-                bookmark.title !== ""
-            ) {
-                return {
-                    content: bookmark.url,
-                    description: bookmark.title,
-                };
-            }
-        });
-
-        const filtered = suggestions.filter(function(x) {
-            return x !== undefined;
-        })
-        suggest(filtered);
-    });
-
-});
+//
+// This (suggest) is currently bugged due to an issue with chromes API:
+// https://bugs.chromium.org/p/chromium/issues/detail?id=1186804
+// Once resolved we can do this again.
+//
+// chrome.omnibox.onInputChanged.addListener((text, suggest) => {
+//     chrome.storage.local.get(["history"], function(items){
+//         if(items?.history) {
+//             const suggestions = JSON.parse(items.history).map((history) => {
+//                 if (history.toLowerCase().includes(text.toLowerCase())) {
+//                     return {
+//                         content: history,
+//                         description: history
+//                     };
+//                 }
+//             });
+//
+//             const filtered = suggestions.filter(function (x) {
+//                 return x !== undefined;
+//             });
+//
+//             suggest(filtered);
+//         }
+//     });
+// });
 
 chrome.omnibox.onInputEntered.addListener((text, OnInputEnteredDisposition) => {
-    if(text.startsWith("http") || text.startsWith("https")) {
-        chrome.tabs.update({url: text});
-    } else {
-        chrome.tabs.update({url: "https://fly.apollorion.com/" + text});
-    }
+
+    chrome.storage.local.get(["history"], function(items){
+        let history;
+        if(items?.history) {
+            history = JSON.parse(items.history);
+            history.push(text);
+        } else {
+            history = [text];
+        }
+
+        chrome.storage.local.set({history: JSON.stringify(history)}, function () {
+            chrome.tabs.update({url: "https://fly.apollorion.com/" + text});
+        });
+    });
+
 });
